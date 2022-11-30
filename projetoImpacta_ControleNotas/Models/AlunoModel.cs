4af617f7147db3fbfc57c4ef22c0902cfe61f2ee
Kauê Ramos Raimundo <kaue.ramos.krr@gmail.com>
2022-11-30 18:34:27 -0300

    Add project files.

---
+++ projetoImpacta_ControleNotas/Models/AlunoModel.cs
@@ -0,0 +1,12 @@
+﻿namespace projetoImpacta_ControleNotas.Models
+{
+    public class AlunoModel
+    {
+        public int ID { get; set; }
+        public string Nome { get; set; }
+        public string Nota1 { get; set; }
+        public string Nota2 { get; set; }
+        public string NotaFinal { get; set; }
+
+    }
+}
